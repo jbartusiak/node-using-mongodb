a6daec904c384d91f97d15ae908c904ef6a142e0
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-08-08 18:19:15 +0200

    Mongoose connection

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {connect} from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
+connect('mongodb://localhost:27017/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
